beaea99c9f8362b1d79bfb43b388e19cc4d13591
James Sinkala <xinnks@gmail.com>
2023-07-21 00:03:00 +0300

    feat: Update function's argument type

--- src/lib/turso.ts
+++ src/lib/turso.ts
@@ -1,7 +1,7 @@
-import type { RequestEventLoader } from "@builder.io/qwik-city";
+import type { RequestEventCommon } from "@builder.io/qwik-city";
 import { createClient, type Client } from "@libsql/client";
 
-export function tursoClient(requestEvent: RequestEventLoader): Client {
+export function tursoClient(requestEvent: RequestEventCommon): Client {
   const url = requestEvent.env.get("TURSO_DB_URL")?.trim();
   if(url === undefined){
     throw new Error("TURSO_DB_URL is not defined")
